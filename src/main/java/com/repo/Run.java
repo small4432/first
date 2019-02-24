baca7ff4ce8491cd26f2ab8d12bf03223dbb11f0
longsheng.chen <longsheng.chen@macrowing.com>
2019-02-24 21:57:09 +0800

    my first project

---
+++ src/main/java/com/repo/Run.java
@@ -0,0 +1,13 @@
+package com.repo;
+
+public class Run {
+
+	public static void main(String[] args) {
+//		System.out.println("hello world");
+		//e为自然常数2.71828
+		System.out.println(Math.exp(1));
+		
+		
+	}
+
+}
